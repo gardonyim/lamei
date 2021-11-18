9d8a4f43d662bbd11c8cccacadb756e9be5b7aae
Márk <mark.gardonyi@gmail.com>
2021-11-18 09:09:13 +0100

    Make reference articles responsive

--- js/main.js
+++ js/main.js
@@ -4,7 +4,14 @@ function init() {
     for (let elem of mainElements) {
         elem.addEventListener('click', menuClick)
     }
-
+    const referenceItems = document.querySelectorAll(".referenceItem");
+    for (let elem of referenceItems) {
+        elem.addEventListener("click", referenceClick)
+    }
+    const backButtons = document.querySelectorAll(".back");
+    for (let button of backButtons) {
+        button.addEventListener("click", goBack)
+    }
 }
 
 function menuClick(event) {
@@ -13,12 +20,32 @@ function menuClick(event) {
         item.style.display = "none";
     }
     let parentElem = this.parentElement;
-    console.log(parentElem.children);
     for (let sibilingOfTarget of parentElem.children) {
         if (sibilingOfTarget.getAttribute("class") === "details") {
-            console.log(sibilingOfTarget);
             sibilingOfTarget.style.display = "block";
         }
+    }
+}
+
+function referenceClick(event) {
+    let menu = document.querySelectorAll(".menu");
+    for (let elem of menu) {
+        elem.style.display = "none";
+    }
+    let referenceId = this.getAttribute("id").slice(7);
+    let referenceArticleId = "#refArticle" + referenceId;
+    let referenceArticle = document.querySelector(referenceArticleId);
+    referenceArticle.style.display = "block";
+}
+
+function goBack(event) {
+    let references = document.querySelectorAll(".reference");
+    for (let item of references) {
+        item.style.display = "none";
+    }
+    let menu = document.querySelectorAll(".menu");
+    for (let elem of menu) {
+        elem.style.display = "block";
     }
 }
 
