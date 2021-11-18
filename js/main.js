15545f2d5a662e3db14df6d406cb5ed31924ed92
Márk <mark.gardonyi@gmail.com>
2021-11-19 00:13:59 +0100

    Refactor main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
 
 function init() {
-    const mainElements = document.querySelectorAll(".menuItem");
-    for (let elem of mainElements) {
-        elem.addEventListener('click', menuClick)
-    }
-    const referenceItems = document.querySelectorAll(".referenceItem");
-    for (let elem of referenceItems) {
-        elem.addEventListener("click", referenceClick)
-    }
-    const backButtons = document.querySelectorAll(".back");
-    for (let button of backButtons) {
-        button.addEventListener("click", goBack)
+    attachEventListeners(".menuItem", menuClick);
+    attachEventListeners(".referenceItem", referenceClick);
+    attachEventListeners(".back", goBack);
+}
+
+function attachEventListeners(selector, eventHandler) {
+    let elemList = document.querySelectorAll(selector);
+    for (let elem of elemList) {
+        elem.addEventListener("click", eventHandler);
     }
 }
 
@@ -19,42 +17,31 @@ function menuClick(event) {
     if (currentDetails.style.display === "block") {
         currentDetails.style.display = "none"
     } else {
-        let listOfDetails = document.querySelectorAll(".details");
-        for (let item of listOfDetails) {
-            item.style.display = "none";
-        }
+        changeVisibility(".details", "none");
         currentDetails.style.display = "block";
     }
-
-
-    // let parentElem = this.parentElement;
-    // parentElem.querySelector(".details").style.display = "block";
-    // for (let sibilingOfTarget of parentElem.children) {
-    //     if (sibilingOfTarget.getAttribute("class") === "details") {
-    //         sibilingOfTarget.style.display = "block";
-    //     }
-    // }
 }
 
 function referenceClick(event) {
-    let menu = document.querySelectorAll(".menu");
-    for (let elem of menu) {
-        elem.style.display = "none";
-    }
-    let referenceId = this.getAttribute("id").slice(7);
+    changeVisibility(".menu", "none");
+    showReference(this.getAttribute("id").slice(7));
+}
+
+function showReference(referenceId) {
     let referenceArticleId = "#refArticle" + referenceId;
     let referenceArticle = document.querySelector(referenceArticleId);
     referenceArticle.style.display = "block";
 }
 
 function goBack(event) {
-    let references = document.querySelectorAll(".reference");
-    for (let item of references) {
-        item.style.display = "none";
-    }
-    let menu = document.querySelectorAll(".menu");
-    for (let elem of menu) {
-        elem.style.display = "block";
+    changeVisibility(".reference", "none");
+    changeVisibility(".menu", "block");
+}
+
+function changeVisibility(selector, visibility) {
+    let elemList = document.querySelectorAll(selector);
+    for (let elem of elemList) {
+        elem.style.display = visibility;
     }
 }
 
