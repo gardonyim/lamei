b800ceb28db787d913bd6f1b831a44a1ce7b88c6
Márk Gárdonyi <mark.gardonyi@gmail.com>
2024-10-03 21:14:02 +0200

    complete redesign

--- js/main.js
+++ js/main.js
@@ -1,47 +1,18 @@
 
 function init() {
-    attachEventListeners(".menuItem", "mouseover", menuClick);
-    attachEventListeners(".referenceItem", "mouseover", referenceClick);
-    attachEventListeners(".back", "click", goBack);
+    // attachEventListeners(".menuItem", "mouseover", menuClick);
+    // attachEventListeners(".referenceItem", "mouseover", referenceClick);
+    // attachEventListeners(".back", "click", goBack);
+    const hamburgerIcon = document.getElementById('hamburger');
+    hamburgerIcon.addEventListener('click', openHamburgerMenu);
 }
 
-function attachEventListeners(selector, eventType, eventHandler) {
-    let elemList = document.querySelectorAll(selector);
-    for (let elem of elemList) {
-        elem.addEventListener(eventType, eventHandler);
-    }
-}
-
-function menuClick(event) {
-    let currentDetails = this.parentElement.querySelector(".details");
-    if (currentDetails.style.display === "block") {
-        currentDetails.style.display = "none"
+function openHamburgerMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " menuOpen";
     } else {
-        changeVisibility(".details", "none");
-        currentDetails.style.display = "block";
-    }
-}
-
-function referenceClick(event) {
-    changeVisibility(".menu", "none");
-    showReference(this.getAttribute("id").slice(7));
-}
-
-function showReference(referenceId) {
-    let referenceArticleId = "#refArticle" + referenceId;
-    let referenceArticle = document.querySelector(referenceArticleId);
-    referenceArticle.style.display = "block";
-}
-
-function goBack(event) {
-    changeVisibility(".reference", "none");
-    changeVisibility(".menu", "block");
-}
-
-function changeVisibility(selector, visibility) {
-    let elemList = document.querySelectorAll(selector);
-    for (let elem of elemList) {
-        elem.style.display = visibility;
+        x.className = "topnav";
     }
 }
 
